cc245b900fc2a9c37849cbccb4859cb6496d2f4f
José Tomás González <jtgonzalez@miuandes.cl>
2018-07-03 01:16:16 -0400

    Code refactor: Cleaned code and managed exceptions

--- speed-script.py
+++ speed-script.py
@@ -1,142 +1,151 @@
-import datetime
-import time
+from datetime import datetime, timedelta
 import cv2
-from collections import deque
 import mysql.connector
 
+PARAM_FILENAME = 'ParametrosSensor'
 
-def connect_to_db(conn_params):
+def main():
+    params_dict = parse_param_file(PARAM_FILENAME)
+
+    camera = cv2.VideoCapture(params_dict['camera_url'])
+
+    reference_frame = get_current_frame(camera)
+
+    reference_frame_time_limit = datetime.now()
+    movement_check_time_limit = reference_frame_time_limit + timedelta(seconds=params_dict['seconds_between_frames'])
+
+    while True:
+        should_check_for_movement = time_limit_expired(movement_check_time_limit)
+
+        if should_check_for_movement:
+            current_frame = get_current_frame(camera)
+
+            movement_detected = detect_movement(reference_frame, current_frame)
+
+            if movement_detected:
+                post_movement_event_to_db(params_dict)
+
+            reference_frame = get_current_frame(camera)
+            reference_frame_time_limit = movement_check_time_limit + timedelta(seconds=params_dict['seconds_between_motion_detection'])
+            movement_check_time_limit = reference_frame_time_limit + timedelta(seconds=params_dict['seconds_between_frames'])
+
+    camera.release()
+
+def parse_param_file(param_filename):
+    params_dict = {}
+
+    params_file = open(param_filename, 'r')
+    for line_number, line in enumerate(params_file):
+
+        if should_ignore_line(line):
+            continue
+
+        try:
+            key, value = line.strip().split('=')
+            params_dict[key] = safe_cast(value)
+            
+        except ValueError:
+            print "Invalid row {0} in file {1}".format(line_number, param_filename)
+            continue
+
+    return params_dict
+
+def should_ignore_line(line):
+    line = line.strip()
+    if len(line) < 3:  # 3 because the min expression is a=b
+        return True
+    elif line[0] == '#':
+        return True
+
+    return False
+
+def safe_cast(value):
+    try:
+        return int(value)
+    except ValueError:
+        pass
+    if value == 'True':
+        return True
+    elif value == 'False':
+        return False
+    return value
+
+def get_current_frame(camera):
+    grabbed = False
+
+    while not grabbed:
+        grabbed, current_frame = camera.read()
+
+    current_frame = cut_camera_frame(current_frame, params_dict)
+
+    current_gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    current_blurred_frame = cv2.GaussianBlur(gray, (21, 21), 0)
+
+    return current_blurred_frame
+
+def cut_camera_frame(frame, params_dict):
+    try:
+        frame = frame[
+            params_dict['camera_top_margin_px']:params_dict['camera_bottom_margin_px'],
+            params_dict['camera_left_margin_px']:params_dict['camera_right_margin_px']
+        ]
+    except KeyError as error:
+        print "[Error] Missing '{0}' parameter in parameters file.".format(error.args[0])
+        exit(1)
+
+    return frame
+
+def detect_movement(reference_frame, frame):
+    contours = get_frame_contours(reference_frame, current_frame)
+
+    for contour in contours:
+        if cv2.contourArea(contour) >= 500:
+            return True
+
+    return False
+
+def get_frame_contours(reference_frame, frame):
+    frame_delta = cv2.absdiff(reference_frame, frame)
+    frame_thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
+    frame_dilate = cv2.dilate(frame_thresh, None, iterations=2)
+
+    contours, _ = cv2.findContours(frame_dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    return contours
+
+def post_movement_event_to_db(params_dict):
+    max_post_tries = params_dict['max_post_tries']
+
+    for _ in range(max_post_tries):
+        connection = connect_to_db(params_dict)
+        post_cursor = connection.cursor()
+
+        post_cursor.execute("INSERT INTO bus_speed (speed) VALUES (1);")
+        connection.commit()
+        print "Guardado: Se detuvo el vehiculo en el tiempo"
+
+        connection.close()
+
+def connect_to_db(params_dict):
     while True:
         try:
             print "[Info] Connecting to db"
-            connection = mysql.connector.connect(buffered=True, **conn_params)
+            connection = mysql.connector.connect(buffered=True,
+                                                 user=params_dict['db_user'],
+                                                 password=params_dict['db_password'],
+                                                 database=params_dict['db_name'],
+                                                 host=params_dict['db_host'])
             print "[Info] Connection successful!"
             return connection
         except mysql.connector.errors.InterfaceError:
             print "[Error] Couldn't connect to database. Retrying..."
             sleep(1)
+        except KeyError as error:
+            print "[Error] Missing '{0}' parameter in parameters file.".format(error.args[0])
+            exit(1)
 
-# Datos para la camara
-camara = cv2.VideoCapture(0)
-time.sleep(0.25)
+def time_limit_expired(time_limit):
+    return time_limit <= datetime.now()
 
-# Datos para actualizar el frame de referencia
-tiempo_frame_referencia = 0
-hora_actualizacion_frame = datetime.datetime.now()
-ReferenceFrame = None
-
-# Datos para mostrar la imagen de la camara
-camara_imagen = False
-x1 = 0
-x2 = 0
-y1 = 0
-y2 = 0
-
-# Datos Conexion base de datos
-usuario = ""
-clave = ""
-nombre_base_de_datos = ""
-
-# Datos para guardar en la base de Datos
-hora_guardar_movimiento_cola = datetime.datetime.now()
-
-# Se inicializan las variables con el documento externo
-archivo = open("ParametrosSensor", 'r')
-for line in archivo:
-    line = line.replace('\n', ' ')
-    elementos = line.split('=')
-    if elementos[0] == "tiempo_frame_referencia":
-        tiempo_frame_referencia = int(elementos[1])
-    elif elementos[0] == "graficos":
-        if elementos[1] == "True":
-            camara_imagen = True
-        else:
-            camara_imagen = False
-    elif elementos[0] == "x1":
-        x1 = int(elementos[1])
-    elif elementos[0] == "x2":
-        x2 = int(elementos[1])
-    elif elementos[0] == "y1":
-        y1 = int(elementos[1])
-    elif elementos[0] == "y2":
-        y2 = int(elementos[1])
-    elif elementos[0] == "usuario":
-        usuario = elementos[1]
-    elif elementos[0] == "clave":
-        clave = elementos[1]
-    elif elementos[0] == "nombre_base_de_datos":
-        nombre_base_de_datos = elementos[1]
-
-CONN_PARAMS = {
-  'user': 'ALPR',
-  'password': 'PASSALPR',
-  'host': 'docker-db',
-  'database': 'control_point'
-}
-
-# Coneccion con base de datos
-connection = connect_to_db(CONN_PARAMS).cursor()
-
-camara_imagen = True
-# Loop infinito para registrar movimiento
-while True:
-    (grabbed, frame) = camara.read()
-    texto = "No hay movimiento"
-    hay_movimiento = False
-
-    if not grabbed:
-        break
-
-    frame = frame[y1:y2, x1:x2]
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
-
-    if hora_actualizacion_frame <= datetime.datetime.now():
-        ReferenceFrame = gray
-        hora_actualizacion_frame += datetime.timedelta(seconds=tiempo_frame_referencia)
-        continue
-
-    frameDelta = cv2.absdiff(ReferenceFrame, gray)
-    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
-
-    thresh = cv2.dilate(thresh, None, iterations=2)
-    (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    for c in cnts:
-        if cv2.contourArea(c) < 500:
-            continue
-        (x, y, w, h) = cv2.boundingRect(c)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        texto = "Hay movimiento"
-        hay_movimiento = True
-
-    if camara_imagen:
-        cv2.putText(frame, "Via: {}".format(texto), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-        cv2.imshow("Security Feed", frame)
-        cv2.imshow("Thresh", thresh)
-        cv2.imshow("Frame Delta", frameDelta)
-        key = cv2.waitKey(1) & 0xFF
-
-        if key == ord("q"):
-            break
-
-    # Se verifica que paso un segundo desde la ultima vez que se guardo si hay movimiento
-    if hora_guardar_movimiento_cola <= datetime.datetime.now():
-        if hay_movimiento:
-            # Hay movimiento
-            # Se guarda de que hubo detencion en la Base de Datos
-            cursor.execute("INSERT INTO bus_speed (speed) VALUES (1);")
-            print "Guardado: Se detuvo el vehiculo en el tiempo"
-
-        elif not hay_movimiento:
-            # No hay movimiento
-            # Se guarda de que no hubo detencion en la Base de Datos
-            cursor.execute("INSERT INTO bus_speed (speed) VALUES (1);")
-            print "Guardado: No se detuvo el vehiculo en el tiempo"
-
-        hora_guardar_movimiento_cola += datetime.timedelta(seconds=1)
-
-# Se corta la conexion con la camara y se destruyen las vistas de esta
-camara.release()
-cv2.destroyAllWindows()
-db.close()
+if __name__ == '__main__':
+    main()
